e7c8a7291a162d83a76f947bf9a68973fdfd82ce
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-06-21 22:09:03 -0700

    declared jquery variables for game.html

---
+++ game-script.js
@@ -0,0 +1,9 @@
+var question = document.getElementById("question");
+var choices = document.getElementsByClassName("choice-text");
+// console.log(choices)
+
+var currentQuestion = {};
+var availableQuestion = [];
+var score = 0;
+var questionCounter = 0;
+var answer = true;
